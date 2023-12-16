0c7ef7c736f77ecc2d4b985e42e49fb0e54da77a
Asif7814 <120505746+Asif7814@users.noreply.github.com>
2023-12-15 21:20:57 -0500

    Restructured JS and updated saveSession and showSession functions.

--- app.js
+++ app.js
@@ -1,65 +1,77 @@
-// will have to restruture parts of this file so that I am able to access the createElement variables properly
+class Task {
+    constructor(text, completed = false) { 
+        this.text = text;
+        this.completed = completed;
+    }
+}
+
+function renderTask (task) {
+    const newTask = document.createElement("li");
+    newTask.setAttribute('class', 'task');
+    taskContainer.appendChild(newTask);
+
+    const newCheckBox = document.createElement("input");
+    newCheckBox.setAttribute('type', 'checkbox');
+    newCheckBox.setAttribute('class', 'task-checkbox');
+    newTask.appendChild(newCheckBox);
+    newCheckBox.checked = task.completed;
+
+    const newLabel = document.createElement("label");
+    newLabel.setAttribute('class', 'task-text');
+    newLabel.innerText = task.text;
+    newLabel.style.textDecoration = task.completed ? "line-through" : "none"; // shortcut to typing out conditional statement (if checked value is on (true), then textDecoration should have a line-through, else the default is off (false) so the textDecoration should be none)
+    newTask.appendChild(newLabel);
+
+    const newDeleteBtn = document.createElement("button");
+    newDeleteBtn.setAttribute('type', 'button');
+    newDeleteBtn.setAttribute('class', 'task-delete');
+    newDeleteBtn.innerText = "X";
+    newTask.appendChild(newDeleteBtn);
+
+    newCheckBox.addEventListener('click', () => { // this is a shortcut to function() {} with some changes - dependent on situation, cannot always be used 
+        task.completed = newCheckBox.checked; // checks the checked value (on or off) and stores value in completed key
+        newLabel.style.textDecoration = task.completed ? "line-through" : "none";
+        saveSession();
+    });
+
+    newDeleteBtn.addEventListener('click', () => {
+        taskContainer.removeChild(newTask);
+        saveSession();
+    });
+}
+
+function addNewTask() {
+    if (text.value == "") {
+        window.alert("Enter in a task!");
+    } else {
+        const newTask = new Task(text.value);
+        renderTask(newTask);
+        text.value = ""; // clear/reset text value from input
+        saveSession();
+    }
+}
+
+function saveSession() {
+    const tasks = Array.from(taskContainer.children).map(taskElement => { // as of the final commit date, I don't fully understand these concepts (Array.from, .map)
+        return new Task( // creates new Task object
+            taskElement.querySelector('.task-text').innerText,
+            taskElement.querySelector('.task-checkbox').checked
+        );
+    });
+
+    localStorage.setItem("current-session", JSON.stringify(tasks));
+}
+
+function showSession() { 
+    const storedTasks = JSON.parse(localStorage.getItem("current-session")) || []; // Retrieve the tasks array from local storage and parse it as JSON
+    storedTasks.forEach(storedTask => renderTask(storedTask)); // for each task that was found in storage, perform the renderTask function to display it on screen
+}
 
 const text = document.getElementById("add-task-text");
 const submitBtn = document.getElementById("add-task-button");
 const taskContainer = document.getElementById("task-container");
 
-function addNewTask() {
-    if (text.value == "") {
-        window.alert("Enter in a task!");
-    } else { 
-        const newTask = document.createElement("li");
-        newTask.setAttribute('class', 'task');
-        taskContainer.appendChild(newTask);
-
-        const newCheckBox = document.createElement("input");
-        newCheckBox.setAttribute('type', 'checkbox');
-        newCheckBox.setAttribute('class', 'task-checkbox');
-        newCheckBox.setAttribute('id', `task-checkbox${1}`);
-        newTask.appendChild(newCheckBox);
-        
-        const newLabel = document.createElement("label");
-        newLabel.setAttribute('for', `task-checkbox${1}`);
-        newLabel.setAttribute('class', 'task-text');
-        newLabel.innerText = text.value;
-        newLabel.style.textDecoration = "none";
-        newTask.appendChild(newLabel);
-
-        const newDeleteBtn = document.createElement("button");
-        newDeleteBtn.setAttribute('type', 'button');
-        newDeleteBtn.setAttribute('class', 'task-delete');
-        newDeleteBtn.innerText = "X";
-        newTask.appendChild(newDeleteBtn);
-
-        text.value = ""; // clear text value from input
-
-        saveSession();
-
-        newCheckBox.addEventListener('click', () => { // this is a shortcut to function() {} with some changes - dependent on situation, cannot always be used 
-            newLabel.style.textDecoration = "line-through";
-            saveSession();
-        });
-
-        newLabel.style.textDecoration = "none";
-
-        newDeleteBtn.addEventListener('click', () => {
-            taskContainer.removeChild(newTask);
-            saveSession();
-        });
-    }
-}
-
-function saveSession() { 
-    localStorage.setItem("current-session", taskContainer.innerHTML);
-}
-
-function showSession() { 
-    taskContainer.innerHTML = localStorage.getItem("current-session");
-}
-
-showSession(); // this shows the locally saved data but it currently cannot be accessed since I've structured the JS where i can only access data if i first call the addNewTask function
-
 text.addEventListener('change', addNewTask); // this allows users to press enter to add new task
 submitBtn.addEventListener('click', addNewTask);
 
-// text.addEventListener('input', addNewTask); - accidental but can be used for live searching
+showSession();
